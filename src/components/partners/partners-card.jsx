e837518a08dd196befd2c6fed59ec29cf3a0cdb5
Yuri <jamesavila540@gmail.com>
2025-11-15 13:38:25 +0800

    feat(update partners page)
    removal of fake data, introduction of our partnered organizations

--- src/components/partners/partners-card.jsx
+++ src/components/partners/partners-card.jsx
@@ -3,8 +3,11 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
 import Image from "next/image";
 import Link from "next/link";
+import { SelectSeparator } from "../ui/select";
 
 const badgeStyle = "bg-fuchsia-900/20 shadow-lg backdrop-blur-lg border border-fuchsia-400/30 text-fuchsia-100"
+const styling = ["w-24 py-4 bg-fuchsia-900/20 hover:bg-fuchsia-700 shadow-lg backdrop-blur-lg border border-fuchsia-400/30 text-fuchsia-100 cursor-pointer transition-all duration-300"]
+
 
 export function PartnersCard({
     name, image, location, des, websiteLink, tags
@@ -12,33 +15,35 @@ export function PartnersCard({
     return (
         <Card className="w-full sm:w-[90%] lg:w-full flex flex-col sm:flex-row items-center sm:items-start justify-between p-4 gap-4 bg-fuchsia-900/20 shadow-lg backdrop-blur-lg border border-fuchsia-400/30 text-fuchsia-100 transition-all duration-300">
             
-            <CardHeader className="flex-shrink-0 w-full sm:w-40 h-40 overflow-hidden rounded-xl flex items-center justify-center">
+            <CardHeader className="shrink-0 w-full sm:w-50 h-40  overflow-hidden rounded-xl flex items-center justify-center">
                 <Image
                     src={image}
                     alt={name}
-                    width={160}
-                    height={160}
-                    className="object-cover w-full h-full"
+                    width={260}
+                    height={260}
+                    className="object-cover w-full h-full bg-fuchsia-500/40 rounded-2xl p-4 border border-fuchsia-300"
                 />
             </CardHeader>
 
-            <CardContent className="flex flex-col flex-1 gap-3 p-0">
-                <div className="flex flex-col justify-start mb-2 sm:mb-4">
-                    <h1 className="text-lg sm:text-xl font-semibold mb-1 sm:mb-2">{name}</h1>
-                    <div className="flex flex-wrap gap-2">
+            <CardContent className="flex flex-col flex-1 gap-4 p-0">
+                <div className="flex flex-col justify-start sm:mb-2">
+                    <h1 className="lg:text-2xl sm:text-xl font-bold mb-1 sm:mb-2">{name}</h1>
+                    <div className="flex flex-wrap gap-2 mb-1 justify-start">
                         {tags.map((tag, idx) => (
                             <Badge key={idx} className={badgeStyle}>{tag}</Badge>
                         ))}
                     </div>
+                    <h2 className="text-sm text-muted-foreground"><b>Location: </b>{location}</h2>
+                    
                 </div>
+                <SelectSeparator  className="bg-fuchsia-700/70"/>
                 <p className="text-sm sm:text-base text-muted-foreground leading-relaxed">
                     {des}
                 </p>
             </CardContent>
 
-            <CardFooter className="w-full sm:w-auto flex flex-col sm:items-end items-start gap-2 mt-4 sm:mt-0">
-                <h2 className="text-sm text-muted-foreground">{location}</h2>
-                <Button size="sm" className="cursor-pointer bg-fuchsia-700 hover:bg-fuchsia-600 text-white border border-fuchsia-600 transition-colors">
+            <CardFooter className="w-full sm:w-auto flex flex-col items-end mt-4 sm:mt-0">
+                <Button size="sm" className={styling}>
                     <Link href={websiteLink} target="_blank" rel="noopener noreferrer">
                         Website
                     </Link>
--- src/pages/partners-page/parnters-page.jsx
+++ src/pages/partners-page/parnters-page.jsx
@@ -2,25 +2,40 @@
 import Threads from "@/components/Threads"
 import { PartnersCard } from "../../components/partners/partners-card"
 
-const fakeData = [
+const data = [
     {
-        name: "Byteon",
-        image: "/images/kaede.jpg",
-        location: "Manila, Philippines",
-        des: "A tech community empowering student developers.",
-        websiteLink: "https://byteon.org",
-        tags: ["education", "tech"]
+        name: "Hack United",
+        image: "/images/hackunited2.png",
+        location: "USA",
+        des: "Empowering the Next Generation of Tech Innovators",
+        websiteLink: "https://www.hackunited.org/",
+        tags: ["tech", "innovation"]
     },
     {
-        name: "Byteon",
-        image: "/images/kaede.jpg",
-        location: "Manila, Philippines",
-        des: "A tech community empowering student developers.",
-        websiteLink: "https://byteon.org",
-        tags: ["education", "tech"]
+        name: "Maximally",
+        image: "/images/Maximallyc.png",
+        location: "India",
+        des: " global innovation league that hosts high-stakes hackathons for ambitious builders. Built by hackers, for hackers.",
+        websiteLink: "https://maximally.in/",
+        tags: ["Innovation", "Hackathon"]
+    },
+    {
+        name: "CS Base",
+        image: "/images/CSBase_logo.png",
+        location: "US",
+        des: "CS Base provides a global ecosystem that enables elementary to high school students to specialize in modern computer science.",
+        websiteLink: "https://www.csbase.org/",
+        tags: ["Tech", "innovation"]
+    },
+    {
+        name: "Medihacks",
+        image: "/images/meddi.png",
+        location: "US",
+        des: "Fostering innovation today for a healthier tommorow.",
+        websiteLink: "https://medihacks.org/",
+        tags: ["Health", "Tech"]
     },
 ]
-
 
 export default function PartnersPage () {
     return (
@@ -32,10 +47,25 @@ export default function PartnersPage () {
                 enableMouseInteraction={false}
                 />
             </div>
+
+            <div className="text-center mt-24">
+                <h1 className="text-4xl sm:text-5xl lg:text-6xl font-bold mb-4">
+                    <span className="text-transparent bg-clip-text bg-gradient-to-r from-fuchsia-300 to-purple-500">
+                        Our Partners
+                    </span>
+                </h1>
+
+                <p className="text-fuchsia-200 max-w-2xl mx-auto text-lg">
+                    Empowering creators, coders, and innovators through community,
+                    learning, and meaningful opportunities.
+                </p>
+            </div>
         
-            {fakeData.map((data, idx) => (
-                <PartnersCard key={idx} {...data} />
-            ))}
+            <div className="flex flex-col gap-2 justify-evenly w-[80%] m-2">
+                {data.map((data, idx) => (
+                    <PartnersCard key={idx} {...data} />
+                ))}
+            </div>
         </div>
     )
 }
