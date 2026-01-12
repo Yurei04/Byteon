6f1051898ee63bb3ca9eb42f36d06568412966e1
Yuri <jamesavila540@gmail.com>
2026-01-12 19:37:15 +0800

    feat(add login and sign up page and components)
    add also supabase ssr dependencies for auth and middleware

--- src/app/(auth)/log-in/page.jsx
+++ src/app/(auth)/log-in/page.jsx
@@ -1,8 +1,11 @@
-export default function LogIn (
+import { LoginForm } from "@/components/login/loginForm";
 
-) {
-    return (
-        <>
-        </>
-    )
+export default function LoginPage() {
+  return (
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  )
 }
