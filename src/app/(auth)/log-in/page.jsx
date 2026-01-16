c3311216022f006a02b37de67dee1d509649186b
Yuri <jamesavila540@gmail.com>
2026-01-16 21:53:34 +0800

    Redesign login and signup pages UI
    Updated the layout and styling of the login and signup pages for improved visual consistency and user experience. The login form now uses framer-motion for hover effects, features a cleaner structure, and includes clearer navigation links. The signup page background color was adjusted for better contrast.

--- src/app/(auth)/log-in/page.jsx
+++ src/app/(auth)/log-in/page.jsx
@@ -1,10 +1,25 @@
-import { LoginForm } from "@/components/login/loginForm";
+import { LoginForm } from "@/components/login/loginForm"
+import { GalleryVerticalEnd } from "lucide-react"
+
+import Image from "next/image"
 
 export default function LoginPage() {
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
+    <div className="grid min-h-svh lg:grid-cols-1 items-center justify-center">
+      <div className="flex flex-col gap-4 p-6 md:p-10 ">
+        <div className="flex justify-center gap-2 md:justify-center">
+          <a href="." className="flex items-center gap-2 font-medium">
+            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+              <GalleryVerticalEnd className="size-4" />
+            </div>
+            Byteon
+          </a>
+        </div>
+        <div className="flex flex-1 items-center justify-center">
+          <div className="w-full max-w-xs">
+            <LoginForm />
+          </div>
+        </div>
       </div>
     </div>
   )
